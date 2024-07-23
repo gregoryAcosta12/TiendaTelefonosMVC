e5a15af1cfe7257480947f2b74146197adf69e42
costa <costa@DESKTOP-EOOHF5T>
2024-07-23 10:54:05 -0400

    Agregar archivos de proyecto.

---
+++ PresentacionTiendaCliente/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PresentacionTiendaCliente
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
